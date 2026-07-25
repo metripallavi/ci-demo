6546f58231a59cee0f9d826619b9e7bfd34e1682
metripallavi <metripallavi17@gmail.com>
2026-07-25 18:46:22 +0530

    Initial CI demo

---
+++ app.py
@@ -0,0 +1,14 @@
+import math
+def add(a, b):
+    """Return the sum of two numbers."""
+    return a + b
+
+
+def subtract(a, b):
+    """Return the difference between two numbers."""
+    return a - b
+
+
+if __name__ == "__main__":
+    print("Addition:", add(10, 20))
+    print("Subtraction:", subtract(20, 5))
